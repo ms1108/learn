5f1dfcfab9f3ef4b4296c5d6522bac194c33381c
mosheng <929262660@qq.com>
2021-07-10 22:07:38 +0800

    learn

--- src/main/java/json/ToJavaBeanTest.java
+++ src/main/java/json/ToJavaBeanTest.java
@@ -10,11 +10,13 @@ public class ToJavaBeanTest {
     @Test
     public void test() {
         JSONObject jsonObject = JSON.parseObject("{\"age\":\"\",\"sex\":{\"testInClass\":2}}");
+        //空字符串会转成null
         User user2 = JSON.toJavaObject(jsonObject, User.class);
         System.out.println(user2);
 
         User user = new User().myUser();
         Map<String,Object> jsonObject1 = JSON.parseObject(JSON.toJSONString(user));
+        System.out.println(jsonObject1);
         jsonObject1.putAll(jsonObject);
         System.out.println(jsonObject1);
         User user1 = JSONObject.parseObject(JSON.toJSONString(jsonObject1), User.class);
